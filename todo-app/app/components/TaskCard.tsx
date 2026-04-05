1948a1254a1a7c03dc2ca4e620ccbf4e29712d7a
GuillermoRJ <g.rdz.jauregui06@gmail.com>
2026-04-04 23:47:01 -0600

    Back and Front connected succesfully

--- todo-app/app/components/TaskCard.tsx
+++ todo-app/app/components/TaskCard.tsx
@@ -1,17 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View } from "react-native";
+import React from "react";
+import { Href, router } from "expo-router";
 
 interface TaskCardProps {
-        id: number,
-        title: string,
-        description: string,
-        completed: boolean,
-        created_at: string
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+  created_at: string;
 }
-export default function TaskCard(props : TaskCardProps) {
+
+export default function TaskCard(props: TaskCardProps) {
   return (
     <View>
-      <Text>{props.title} </Text>
+      <Text>{props.id}</Text>
+      <Text>{props.title}</Text>
+      <Text>{props.description}</Text>
+      <Text>{props.completed ? "Completado" : "Pendiente"}</Text>
+      <Text>{props.created_at}</Text>
     </View>
-  )
+  );
 }
--- todo-app/app/index.tsx
+++ todo-app/app/index.tsx
@@ -1,26 +1,35 @@
-import { ScrollView, Text } from "react-native";
+import { ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
 import TaskCard from "./components/TaskCard";
 
-export default function index() {
+export default function Index() {
   const [results, setResults] = useState<any[]>([]);
+
+  const getTasks = async () => {
+    const URL = "http://localhost:3000/todos";
+
+    try {
+      const response = await fetch(URL, {
+        method: "GET",
+      });
+
+      const jsonResponse = await response.json();
+
+      setResults(jsonResponse.data);
+    } catch (error) {
+      console.error("Error al obtener las tareas:", error);
+    }
+  };
 
   useEffect(() => {
     getTasks();
   }, []);
 
-  const getTasks = async () => {
-    const URL = "http://localhost:3000";
-    const response = await fetch(URL, {
-      method: "GET",
-    });
-    const data = await response.json();
-    setResults(data.results);
-    console.log(data);
-  };
   return (
     <ScrollView>
-      <Text>ola</Text>
+      {results?.map((task) => (
+        <TaskCard key={task.id} {...task} />
+      ))}
     </ScrollView>
   );
 }
